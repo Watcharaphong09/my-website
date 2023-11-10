3ed02071454c084bae284e18a6b7632841dbc76d
Watcharaphong09 <csmos11450qw@gmail.com>
2023-11-10 08:39:34 +0700

    fix page

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -16,7 +16,7 @@ const About = () => {
 
     return (
         <>
-            <div id="Skills" className="mb-20 h-full flex flex-col items-center justify-center dark:bg-swhite">
+            <div id="Skills" className="pb-20 h-full flex flex-col items-center justify-center dark:bg-swhite">
                 <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-20 leading-normal uppercase text-cyan-300 dark:text-yellow-300 underline decoration-wavy decoration-white dark:decoration-black decoration-4 underline-offset-4">Skills</h1>
                 <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 justify-around gap-10 md:gap-20">
                     <h2 data-aos="fade-up" className="text-7xl flex items-center justify-center font-semibold text-cyan-500 dark:text-yellow-300 rounded-3xl h-36 w-44 border-2 border-cyan-500 dark:border-yellow-500 shadow-bglow dark:shadow-wglow"> <AiFillHtml5 /> </h2>
